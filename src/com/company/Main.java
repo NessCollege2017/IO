188e575113d1cc1dbfb09c5f73809b6a47ce6407
NessCollege <nesscollege2017@gmail.com>
2017-05-12 13:06:40 +0300

    website, refactored the read method out

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        try {
+            System.out.println(StreamIO.readWebSite("http://www.omdbapi.com/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/com/company/StreamIO.java
+++ src/com/company/StreamIO.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -9,8 +10,8 @@ import java.net.URLConnection;
  */
 public class StreamIO {
 
-    public static void write(String fileName, String data){
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))){
+    public static void write(String fileName, String data) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             //2) call the write method.
             writer.write(data);
             //writer.newLine();
@@ -19,31 +20,23 @@ public class StreamIO {
         }
     }
 
-    public static String read(String fileName){
-        StringBuilder data = new StringBuilder();
+    public static String read(String fileName) throws IOException {
         //data = content read the file
         //init a file reader.
-        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
-            String line;
-            while ((line = reader.readLine())!=null){
-                data.append(line).append(System.lineSeparator());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return data.toString();
+        InputStream in  = new FileInputStream(fileName);
+        return read(in);
     }
 
     //copy
-    public static void copy(String srcFileName, String destFileName){
+    public static void copy(String srcFileName, String destFileName) {
         try (InputStream in = new FileInputStream(srcFileName);
-             OutputStream out = new FileOutputStream(destFileName)){
+             OutputStream out = new FileOutputStream(destFileName)) {
 
             byte[] buffer = new byte[8192];
             int length = 0;
 
-            while ((length = in.read(buffer, 0, buffer.length)) !=-1){
-                out.write(buffer, 0 ,buffer.length);
+            while ((length = in.read(buffer, 0, buffer.length)) != -1) {
+                out.write(buffer, 0, buffer.length);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -52,16 +45,32 @@ public class StreamIO {
 
     //read from the web
     //https://www.reddit.com/r/Android/.json
-    public static String readWebSite(String address){
-        URL url = new URL(address);
+    public static String readWebSite(String address) throws IOException {
+        URL url = null;
+        try {
+            url = new URL(address);
+        } catch (MalformedURLException e) {
+            throw new IOException("failed at parsing the URL.", e);
+        }
         URLConnection con = url.openConnection();
         InputStream in = con.getInputStream();
+        return read(in);
+    }
 
+    public static String read(InputStream in) throws IOException {
+        StringBuilder builder = new StringBuilder();//mutable String (appendable String)
         //A) wrap the inputStream in an InputStreamReader
-
         //B) wrap the reader in a buffered reader
+        //C)read the data
+        //D)close everything
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
 
+            String line = null;
 
-        //do the same as read file.
+            while ((line = reader.readLine()) != null) {
+                builder.append(line).append(System.lineSeparator());
+            }
+        }
+        return builder.toString();
     }
 }
